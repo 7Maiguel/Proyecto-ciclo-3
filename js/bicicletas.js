d9c06ec0f6727829fd435dfb96afa5611489568d
7Maiguel <miguel1235.mar@gmail.com>
2022-09-26 20:44:17 -0500

    Formulario bicicletas completo con funcionalidad

--- js/bicicletas.js
+++ js/bicicletas.js
@@ -5,14 +5,25 @@ function traerInformacion(){
         url:BASE_URL,
         type:"GET",
         dataType:"JSON",
-        success:function (respuesta){
+        success: function(respuesta){
             console.log(respuesta);
-            //pintarRespuesta(respuesta.items);
+            pintarRespuesta(respuesta.items);
         }
-    });
+    })
 }
-    function pintarRespuesta(items){
-    let myTable= "<table>";
+
+function pintarRespuesta(items){
+    let myTable= 
+    `<table style="border: 1px solid black">
+        <thead>
+            <th>ID</th>
+            <th>Marca</th>
+            <th>Modelo</th>
+            <th>Categoria</th>
+            <th>Nombre</th>
+        </thead>
+        <tbody>`;
+    
     for (let i=0; i<items.length;i++){
         myTable+="<tr>";
         myTable+="<td>"+items[i].id+"</td>";
@@ -23,25 +34,28 @@ function traerInformacion(){
         myTable+="<td> <button onclick='borrarElemento("+items[i].id+")'>Borrar</button>";
         myTable+="</tr>";
     }
-    myTable+="</table>";
+    myTable+="</tbody></table>";
+    $("#resultado").empty();
     $("#resultado").append(myTable);
 }
-    function guardarInformacion(){
+
+function guardarInformacion(){
     let myData={
         id:$("#id").val(),
         brand:$("#brand").val(),
         model:$("#model").val(),
         category_id:$("#category_id").val(),
-        name:$("#name").val(),
+        name:$("#name").val()
     };
+
     let dataToSend=JSON.stringify(myData);
+    
     $.ajax({
-        url:"https://g8c991693a4d3f3-jfpiwsxg2od4f43e.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/bike/bike\n",
+        url:BASE_URL,
         type:"POST",
-        data:myData,
-        dataType:"JSON",
+        data:dataToSend,
+        contentType: 'application/json',
         success:function(respuesta){
-            $("#resultado").empty();
             $("#id").val("");
             $("#brand").val("");
             $("#model").val("");
@@ -51,8 +65,9 @@ function traerInformacion(){
             alert("Se ha guardado el dato");
         }
     });
-    }
-    function editarInformacion(){
+}
+
+function editarInformacion(){
     let myData={
         id:$("#id").val(),
         brand:$("#brand").val(),
@@ -60,41 +75,41 @@ function traerInformacion(){
         category_id:$("#category_id").val(),
         name:$("#name").val(),
     };
-    console.log(myData);
+
     let dataToSend=JSON.stringify(myData);
+    
     $.ajax({
-        url:"https://g8c991693a4d3f3-jfpiwsxg2od4f43e.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/bike/bike\n",
+        url:BASE_URL,
         type:"PUT",
         data:dataToSend,
-        contentType:"application/JSON",
-        dataType:"JSON",
+        contentType:"application/json",
         success:function(respuesta){
-            $("#resultado").empty();
             $("#id").val("");
             $("#brand").val("");
             $("#model").val("");
             $("#category_id").val("");
             $("#name").val("");
             traerInformacion();
-            alert("Se ha actualizado");
+            alert("Se ha actualizado un dato");
         }
     });
-    }
-    function borrarElemento(idElemento){
+}
+
+function borrarElemento(idElemento){
     let myData={
         id:idElemento
         };
+
     let dataToSend=JSON.stringify(myData);
+
     $.ajax({
-        url:"https://g8c991693a4d3f3-jfpiwsxg2od4f43e.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/bike/bike\n",
+        url:BASE_URL,
         type:"DELETE",
         data:dataToSend,
-        contentType: "application/JSON",
-        dataType:"JSON",
+        contentType: "application/json",
         success:function(respuesta){
-            $("#resultado").empty();
             traerInformacion();
-            alert("Se ha eliminado");
+            alert("Se ha eliminado un dato");
         }
     });
-    }
+}
